3389ee20472290ed9b899a8345b2215eeaed9cc0
Rijjit <114848504+Rijjit@users.noreply.github.com>
2025-08-11 21:31:05 +0530

    28043

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(request):
+    context ={
+        "restaurant_name" : "The Sebs'"
+    }
+    return render(request, "home-html", context)
