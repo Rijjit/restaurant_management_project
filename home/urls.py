548cf727960bcef159fc4015bb29cc03ffc1f885
Rijjit <114848504+Rijjit@users.noreply.github.com>
2025-08-12 17:10:48 +0530

    28042

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', views.home_page, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('menu/', views.menu, name="menu"),
+    path('about/', views.about, name="about"),
 ]
